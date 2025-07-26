1a5c81711cd754cf762425cb9c4222822d11e443
devcom33 <devcom33@users.noreply.github.com>
2025-07-26 20:26:47 +0100

    fix some issue in order to implement updates

--- backend/src/main/java/org/heymouad/focusapp/controllers/HabitController.java
+++ backend/src/main/java/org/heymouad/focusapp/controllers/HabitController.java
@@ -3,14 +3,15 @@ package org.heymouad.focusapp.controllers;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.heymouad.focusapp.dtos.HabitDto;
+import org.heymouad.focusapp.dtos.HabitUpdateRequest;
 import org.heymouad.focusapp.entities.Habit;
 import org.heymouad.focusapp.mappers.HabitMapper;
+import org.heymouad.focusapp.repositories.HabitRepository;
 import org.heymouad.focusapp.services.HabitService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequestMapping("/api")
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 public class HabitController {
     private final HabitService habitService;
     private final HabitMapper habitMapper;
+    private final HabitRepository habitRepository;
 
     @PostMapping("/addHabit")
     public ResponseEntity<Habit> saveHabit(@RequestBody HabitDto habitRequestDto)
@@ -31,13 +33,18 @@ public class HabitController {
     }
 
     @GetMapping("/getHabits")
-    public ResponseEntity<List<HabitDto>> getAllHabits()
+    public ResponseEntity<List<Habit>> getAllHabits()
     {
-        List<HabitDto> habitList = habitService.getAllHabit()
-                .stream()
-                .map(habitMapper::toHabitDto)
-                .collect(Collectors.toList());
+        List<Habit> habitList = habitService.getAllHabit();
 
         return ResponseEntity.ok(habitList);
     }
+
+    @PatchMapping("/updateHabit/{id}")
+    public ResponseEntity<Habit> updateHabit(@PathVariable Long id, @RequestBody HabitUpdateRequest habitUpdateRequest)
+    {
+        Habit updatedHabit = habitService.updateHabit(id, habitUpdateRequest.completed());
+        return ResponseEntity.ok(updatedHabit);
+    }
+
 }
