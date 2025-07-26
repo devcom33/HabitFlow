cafec41762d7dc801fa9b0f8921349d4dca574df
devcom33 <devcom33@users.noreply.github.com>
2025-07-26 12:44:24 +0100

    add toHabitDto on the mapper

--- backend/src/main/java/org/heymouad/focusapp/mappers/HabitMapper.java
+++ backend/src/main/java/org/heymouad/focusapp/mappers/HabitMapper.java
@@ -1,11 +1,12 @@
 package org.heymouad.focusapp.mappers;
 
 
-import org.heymouad.focusapp.dtos.HabitRequestDto;
+import org.heymouad.focusapp.dtos.HabitDto;
 import org.heymouad.focusapp.entities.Habit;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface HabitMapper {
-    Habit toHabit(HabitRequestDto habitRequestDto);
+    Habit toHabit(HabitDto habitDto);
+    HabitDto toHabitDto(Habit habit);
 }
