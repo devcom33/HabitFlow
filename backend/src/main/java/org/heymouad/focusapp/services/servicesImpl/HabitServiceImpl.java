15f99bdb78c1a9e4cf0de49ade41a56767a8b25e
devcom33 <devcom33@users.noreply.github.com>
2025-07-27 22:39:12 +0100

    add a reset daily habits schedule

--- backend/src/main/java/org/heymouad/focusapp/services/servicesImpl/HabitServiceImpl.java
+++ backend/src/main/java/org/heymouad/focusapp/services/servicesImpl/HabitServiceImpl.java
@@ -2,15 +2,18 @@ package org.heymouad.focusapp.services.servicesImpl;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.heymouad.focusapp.entities.Habit;
 import org.heymouad.focusapp.repositories.HabitRepository;
 import org.heymouad.focusapp.services.HabitService;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class HabitServiceImpl implements HabitService {
     private final HabitRepository habitRepository;
 
@@ -33,5 +36,17 @@ public class HabitServiceImpl implements HabitService {
     @Override
     public List<Habit> getAllHabit() {
         return habitRepository.findAll();
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void resetDailyHabits() {
+        log.info("Running daily habit reset...");
+        List<Habit> habits = getAllHabit();
+        habits.forEach(habit -> {
+            if (habit.isCompleted())
+            {
+                habit.setCompleted(false);
+            }
+        });
     }
 }
