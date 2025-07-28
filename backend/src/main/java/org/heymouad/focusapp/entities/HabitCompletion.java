91cad0cf6dbe96995d76f0a9c3796ae4757f9b1b
devcom33 <devcom33@users.noreply.github.com>
2025-07-28 22:12:54 +0100

    add new entity to specialize on just completion habit management

---
+++ backend/src/main/java/org/heymouad/focusapp/entities/HabitCompletion.java
@@ -0,0 +1,40 @@
+package org.heymouad.focusapp.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class HabitCompletion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "habit_id", nullable = false)
+    private Habit habit;
+
+    private LocalDate completionDate;
+    private LocalDateTime completionTime = LocalDateTime.now();
+
+    @PrePersist
+    protected void onCreate() {
+        if (this.completionTime == null) {
+            this.completionTime = LocalDateTime.now();
+        }
+        if (this.completionDate == null) {
+            this.completionDate = LocalDate.now();
+        }
+    }
+
+}
