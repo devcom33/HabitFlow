c348d8952579a16c427b086724fe4d95010f4998
devcom33 <devcom33@users.noreply.github.com>
2025-09-08 23:45:59 +0100

    add an url to login ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./pages/dashboard";
 import HabitDetails from "./pages/habitDetails";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/habit" element={<HabitDetails />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
