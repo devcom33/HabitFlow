f6a05345757b8b4efdfd3a6110dc24313b04a9c1
devcom33 <devcom33@users.noreply.github.com>
2025-09-13 22:13:21 +0100

    update data annotation

--- backend/src/main/java/org/heymouad/focusapp/entities/Habit.java
+++ backend/src/main/java/org/heymouad/focusapp/entities/Habit.java
@@ -2,23 +2,22 @@ package org.heymouad.focusapp.entities;
 
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Table(name = "habit")
 @Builder
 public class Habit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    //@Table(name = "habit", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", name}))
     private String name;
     private LocalDate createdAt;
 
