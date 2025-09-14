e6b0c97dd54b64d1d9c59dda5e75bfe52d867171
devcom33 <devcom33@users.noreply.github.com>
2025-09-14 14:05:46 +0100

    add roleseeder for create roles in startup

---
+++ backend/src/main/java/org/heymouad/focusapp/config/RoleSeeder.java
@@ -0,0 +1,25 @@
+package org.heymouad.focusapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.heymouad.focusapp.entities.AppRole;
+import org.heymouad.focusapp.enums.RoleName;
+import org.heymouad.focusapp.repositories.AppRoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RoleSeeder implements CommandLineRunner {
+    private final AppRoleRepository appRoleRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        if (appRoleRepository.findByRole(RoleName.USER).isEmpty())
+        {
+            appRoleRepository.save(new AppRole(null, RoleName.USER));
+        }
+        if (appRoleRepository.findByRole(RoleName.ADMIN).isEmpty())
+        {
+            appRoleRepository.save(new AppRole(null, RoleName.ADMIN));
+        }
+    }
+}
