ea339c8297e606b3a54b0e6ac6358a598965ff28
devcom33 <devcom33@users.noreply.github.com>
2025-09-14 14:45:54 +0100

    add roles and validation annotations

--- backend/src/main/java/org/heymouad/focusapp/dtos/RegisterRequest.java
+++ backend/src/main/java/org/heymouad/focusapp/dtos/RegisterRequest.java
@@ -1,18 +1,34 @@
 package org.heymouad.focusapp.dtos;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.heymouad.focusapp.entities.AppRole;
+import org.heymouad.focusapp.enums.RoleName;
+
+import java.util.Set;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank
     private String firstname;
+
+    @NotBlank
     private String lastname;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
+
+    private Set<RoleName> roles;
 }
