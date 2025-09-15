889bf29b3c464cb58e9f61122d894027234af634
devcom33 <devcom33@users.noreply.github.com>
2025-09-15 22:09:08 +0100

    update to fetch-eager

--- backend/src/main/java/org/heymouad/focusapp/entities/AppUser.java
+++ backend/src/main/java/org/heymouad/focusapp/entities/AppUser.java
@@ -25,12 +25,17 @@ public class AppUser implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstname;
+
     private String lastname;
+
     private String email;
+
     @JsonIgnore
     private String password;
-    @ManyToMany
+
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
     joinColumns = @JoinColumn(name = "user_id"),
     inverseJoinColumns = @JoinColumn(name = "role_id")
