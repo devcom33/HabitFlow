e8e853a4c2efa4ba158db58e68a564b1805f2b88
devcom33 <devcom33@users.noreply.github.com>
2025-09-23 19:26:47 +0100

    add loading to protectedroute

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -2,11 +2,10 @@ import { useAuth } from "../store/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
+  if (loading) return <div>Loading...</div>;
+  if (!user) return <Navigate to="/login" replace />;
 
   return children;
 }
