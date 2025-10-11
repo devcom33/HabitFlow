c86a71c567ef0bb585481a3fff5892f2d8ccbce3
devcom33 <devcom33@users.noreply.github.com>
2025-10-11 17:05:15 +0100

    add service logic update settings

--- backend/src/main/java/org/heymouad/focusapp/services/AppUserService.java
+++ backend/src/main/java/org/heymouad/focusapp/services/AppUserService.java
@@ -1,9 +1,13 @@
 package org.heymouad.focusapp.services;
 
+import org.heymouad.focusapp.dtos.AppUserResponse;
+import org.heymouad.focusapp.dtos.UserInfoRequest;
 import org.heymouad.focusapp.entities.AppUser;
 
 public interface AppUserService {
     AppUser getUserDetails(Long id);
 
     AppUser getUserByEmail(String email);
+
+    AppUserResponse updateUserSettings(String currentUserEmail, UserInfoRequest userInfoRequest);
 }
--- backend/src/main/java/org/heymouad/focusapp/services/servicesImpl/AppUserServiceImpl.java
+++ backend/src/main/java/org/heymouad/focusapp/services/servicesImpl/AppUserServiceImpl.java
@@ -1,6 +1,9 @@
 package org.heymouad.focusapp.services.servicesImpl;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.heymouad.focusapp.dtos.AppUserResponse;
+import org.heymouad.focusapp.dtos.UserInfoRequest;
 import org.heymouad.focusapp.entities.AppUser;
 import org.heymouad.focusapp.repositories.AppUserRepository;
 import org.heymouad.focusapp.services.AppUserService;
@@ -19,6 +22,28 @@ public class AppUserServiceImpl implements AppUserService {
 
     @Override
     public AppUser getUserByEmail(String email) {
-        return appUserRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found."));
+        return appUserRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found: "+ email));
+    }
+
+    @Transactional
+    public AppUserResponse updateUserSettings(String currentUserEmail, UserInfoRequest userInfoRequest)
+    {
+        AppUser appUser = getUserByEmail(currentUserEmail);
+
+        if (userInfoRequest.firstname() != null && !userInfoRequest.firstname().isBlank()) {
+            appUser.setFirstname(userInfoRequest.firstname());
+        }
+
+        if (userInfoRequest.lastname() != null && !userInfoRequest.lastname().isBlank()) {
+            appUser.setLastname(userInfoRequest.lastname());
+        }
+
+        if (userInfoRequest.email() != null && !userInfoRequest.email().isBlank()) {
+            appUser.setEmail(userInfoRequest.email());
+        }
+
+        AppUser updatedUser = appUserRepository.save(appUser);
+
+        return AppUserResponse.from(updatedUser);
     }
 }
