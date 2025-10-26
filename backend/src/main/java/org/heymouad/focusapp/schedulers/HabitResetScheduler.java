f0dc09b66bf5c75fd71916f0f2c738a35cbafc28
devcom33 <devcom33@users.noreply.github.com>
2025-10-26 13:36:25 +0100

    chore: update the scheduler to run every minute

--- backend/src/main/java/org/heymouad/focusapp/schedulers/HabitResetScheduler.java
+++ backend/src/main/java/org/heymouad/focusapp/schedulers/HabitResetScheduler.java
@@ -15,7 +15,7 @@ public class HabitResetScheduler {
         this.habitResetService = habitResetService;
     }
 
-    @Scheduled(cron = "0 26 14 * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void runDailyReset() {
         habitResetService.resetDailyHabits(LocalDate.now());
     }
