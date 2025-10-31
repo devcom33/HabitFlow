3d00afbe36efab75d11538f3288223ea559a4927
devcom33 <devcom33@users.noreply.github.com>
2025-10-31 21:46:53 +0100

    feat: add /last-7-days-completions endpoint

--- backend/src/main/java/org/heymouad/focusapp/controllers/DashboardController.java
+++ backend/src/main/java/org/heymouad/focusapp/controllers/DashboardController.java
@@ -1,11 +1,26 @@
 package org.heymouad.focusapp.controllers;
 
 
+import lombok.RequiredArgsConstructor;
+import org.heymouad.focusapp.dtos.HabitCompletionCountDTO;
+import org.heymouad.focusapp.services.DashboardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RequestMapping("/api/dashboard")
 @RestController
+@RequiredArgsConstructor
 public class DashboardController {
+    private final DashboardService dashboardService;
+
+    @GetMapping("/last-7-days-completions")
+    public ResponseEntity<List<HabitCompletionCountDTO>> getLast7DaysCompletions()
+    {
+        return ResponseEntity.ok(dashboardService.getLast7DaysCompletions());
+    }
 }
