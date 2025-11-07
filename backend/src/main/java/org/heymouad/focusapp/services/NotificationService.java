f524b94818bde1594b1eefc32da7375d69fa2775
devcom33 <devcom33@users.noreply.github.com>
2025-11-07 21:44:21 +0100

    feat: update to userEmail instead of userId

--- backend/src/main/java/org/heymouad/focusapp/services/NotificationService.java
+++ backend/src/main/java/org/heymouad/focusapp/services/NotificationService.java
@@ -3,5 +3,5 @@ package org.heymouad.focusapp.services;
 import org.heymouad.focusapp.entities.Notification;
 
 public interface NotificationService {
-    void sendNotification(String userId, Notification notification);
+    void sendNotification(String userEmail, Notification notification);
 }
