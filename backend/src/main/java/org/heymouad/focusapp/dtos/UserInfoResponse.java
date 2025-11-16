9e83bc2374b63e4fe9336abb69357e7c00aa7582
devcom33 <devcom33@users.noreply.github.com>
2025-11-16 18:42:13 +0100

    refactor: add more user info response

--- backend/src/main/java/org/heymouad/focusapp/dtos/UserInfoResponse.java
+++ backend/src/main/java/org/heymouad/focusapp/dtos/UserInfoResponse.java
@@ -1,7 +1,12 @@
 package org.heymouad.focusapp.dtos;
 
+import java.time.LocalDate;
+
 public record UserInfoResponse(
+        String username,
         String firstname,
         String lastname,
-        String email) {
+        String email,
+        String gender,
+        LocalDate createdAt) {
 }
